1411029b5fcca3cb04ea50b2eebab8e28ed6ad12
EVCOMPASSS <support@evcompass.in>
2026-09-24 11:00:36 +0530

    Add sitemap function for metadata route

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,13 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://evcompass.in',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
 
